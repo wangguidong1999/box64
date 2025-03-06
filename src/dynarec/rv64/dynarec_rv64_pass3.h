5f2228db63d064c3cf5e828f8cfb455f2776ab86
wgd <wanggd2022@shanghaitech.edu.cn>
2025-03-06 19:17:26 +0800

    [DEBUG] Rollback to the last LOOKUP_TABLE

--- src/dynarec/rv64/dynarec_rv64_pass3.h
+++ src/dynarec/rv64/dynarec_rv64_pass3.h
@@ -42,18 +42,28 @@
         AUIPC(x1, SPLIT20(val64offset));            \
         FLD(A, x1, SPLIT12(val64offset));           \
     }
-#define LOOKUP_TABLE(A, V) do {                     \
-    uintptr_t addr = (V);                           \
-    uint32_t offset_hi = SPLIT20((uintptr_t)addr);  \
-    uint32_t offset_lo = SPLIT12((uintptr_t)addr);  \
-    if ((offset_lo & 0x800) != 0) {                 \
-        offset_hi += 1;                             \
-    }                                               \
-    LUI(A, offset_hi);                              \
-    if (offset_lo != 0) {                           \
-        ADDI(A, A, offset_lo);                      \
-    }                                               \
+#define LOOKUP_TABLE(A, V) do {                   \
+    uintptr_t addr = (V);                              \
+    uint32_t offset_hi = SPLIT20((uintptr_t)addr);      \
+    uint32_t offset_lo = SPLIT12((uintptr_t)addr);      \
+    AUIPC(A, offset_hi);                               \
+    if (offset_lo != 0) {                              \
+        ADDI(A, A, offset_lo);                         \
+    }                                                  \
 } while(0)
+
+// #define LOOKUP_TABLE(A, V) do {                     \
+//     uintptr_t addr = (V);                           \
+//     uint32_t offset_hi = SPLIT20((uintptr_t)addr);  \
+//     uint32_t offset_lo = SPLIT12((uintptr_t)addr);  \
+//     if ((offset_lo & 0x800) != 0) {                 \
+//         offset_hi += 1;                             \
+//     }                                               \
+//     LUI(A, offset_hi);                              \
+//     if (offset_lo != 0) {                           \
+//         ADDI(A, A, offset_lo);                      \
+//     }                                               \
+// } while(0)
 
 
 #define DEFAULT_VECTOR                                                                                                                  \
